83998afc76071b53e8702c970b8849c8a050e855
Alexander <Alexander.Friedrichsen7@gmail.com>
2024-11-25 16:34:38 +0100

    added popup menu for Task and Employee.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,55 @@
 
-//popup
+
+//popup Scripts
+//popupCustomer
 document.addEventListener('DOMContentLoaded', function() {
-  let popup = document.getElementById('popup');
-  let popupButton = document.getElementById('popupButton');
+  let popupCustomer = document.getElementById('idPopupCustomer');
+  let popupButtonCustomer = document.getElementById('popupButtonCostumer');
 
-  function openPopup() {
-    popup.classList.add("open-popup");
+  function openCustomerPopup() {
+    popupCustomer.classList.add("open-popupCustomer");
   }
 
-  function closePopup() {
-    popup.classList.remove("open-popup");
+  function closeCustomerPopup() {
+    popupCustomer.classList.remove("open-popupCustomer");
   }
 
-  popupButton.addEventListener('click', openPopup);
-  document.querySelector('.close-popup-button').addEventListener('click', closePopup);
+  popupButtonCustomer.addEventListener('click', openCustomerPopup);
+  document.querySelector('.close-popup-customer-button').addEventListener('click', closeCustomerPopup);
+});
+
+
+
+//popupTask
+document.addEventListener('DOMContentLoaded', function() {
+  let popupTask = document.getElementById('idPopupTask');
+  let popupButtonTask = document.getElementById('popupButtonTask');
+
+  function openTaskPopup() {
+    popupTask.classList.add("open-popupTask");
+  }
+
+  function closeTaskPopup() {
+    popupTask.classList.remove("open-popupTask");
+  }
+
+  popupButtonTask.addEventListener('click', openTaskPopup);
+  document.querySelector('.close-popup-task-button').addEventListener('click', closeTaskPopup);
+});
+
+//popupEmployee
+document.addEventListener('DOMContentLoaded', function() {
+  let popupEmployee = document.getElementById('idPopupEmployee');
+  let popupButtonEmployee = document.getElementById('popupButtonEmployee');
+
+  function openEmployeePopup() {
+    popupEmployee.classList.add("open-popupEmployee");
+  }
+
+  function closeEmployeePopup() {
+    popupEmployee.classList.remove("open-popupEmployee");
+  }
+
+  popupButtonEmployee.addEventListener('click', openEmployeePopup);
+  document.querySelector('.close-popup-Employee-button').addEventListener('click', closeEmployeePopup);
 });
